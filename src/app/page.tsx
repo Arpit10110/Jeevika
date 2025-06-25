be934824e86f6d084f56fe30a9a7e1d9b79bcf2b
Arpit <omagrahari55@gmail.com>
2025-06-25 22:26:49 +0530

    section 4 done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import HeroSection from '@/components/HeroSection';
 import Section2 from '@/components/Section2';
 import Section3 from '@/components/Section3';
+import Section4 from '@/components/Section4';
 const page = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const page = () => {
         <HeroSection/>
         <Section2/>
         <Section3/>
+        <Section4/>
     </>
   )
 }
---
+++ src/components/Section4.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+import Sec4Image1 from "@/assets/Sec4Image1.png"
+const Section4 = () => {
+  const steps=[
+    "Upload a clear photo of your prescription",
+    "Your prescription is shared with nearby pharmacies",
+    "Receive price quotes from multiple stores",
+    "Select the best offer and place your order"
+  ]
+  return (
+    <>
+        <div className='w-full  flex felx-wrap justify-around  py-[8rem] items-center ' >  
+            <div className='w-[50%] flex flex-col gap-[1rem] ' >
+              <div className='flex flex-col gap-[1rem] ' >
+                <h2 className='text-[3rem]  font-bold ' >📷 Don't want to type? Just upload your prescription.</h2>
+                <p className='text-[1.5rem] font-semibold text-gray-700 ' >Snap a photo of your prescription and upload it. We'll share it with nearby medical shops, and you'll receive price quotes from multiple pharmacies. Choose the best deal and place your order with confidence.</p>
+              </div>
+              <div className='bg-blue-100 w-[90%] py-[2rem] rounded-[0.5rem] px-[1rem] flex gap-[1.5rem] flex-col   ' >
+                {
+                  steps.map((i,index)=>{
+                    return(
+                      <h3 key={index} className='text-[1.5rem] font-semibold ' ><span className='text-blue-700 bg-[#adbff7e3] rounded-[50%] px-[0.9rem]  py-[0.2rem] ' >{index+1}</span> {i}</h3>
+                    )
+                  })
+                }
+              </div>
+            </div>
+            <div className='w-[40%]' >
+                <Image className='w-full' src={Sec4Image1} alt=''   />
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Section4
