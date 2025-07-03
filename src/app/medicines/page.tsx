72e8dc68050155aa0f1585c682171d6a50fcc8df
Arpit <omagrahari55@gmail.com>
2025-07-03 23:40:13 +0530

    product fetching and product card is complered

--- src/app/medicines/page.tsx
+++ src/app/medicines/page.tsx
@@ -1,15 +1,39 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import SearchIcon from '@mui/icons-material/Search';
 import Link from 'next/link';
+import axios from 'axios';
+import Image from 'next/image';
+import default_image from "@/assets/default_image.jpg"
+import Footer from '@/components/Footer';
 const page = () => {
 
     const [SerachInput, setSerachInput] = useState<string>("");
+    const [MedData,SetMedData] = useState<any[]>([]);
 
     const searchmed = async()=>{
         console.log(SerachInput);
     }
+
+
+    const fetch_med = async()=>{
+        try {
+            const res= await axios.get("http://localhost:5000/medrouter/getallmedicines")
+            console.log(res.data);
+            SetMedData(res.data.data);
+        } catch (error) {
+            
+        }
+    }
+
+
+    useEffect(() => {
+        fetch_med()
+    }, [])
+    
+
+
 
   return (
     <>
@@ -26,7 +50,33 @@ const page = () => {
                 </div>
                     <Link href={"/"} className='bg-[#00beb1] text-white py-[0.5rem] px-[1rem] text-[1.8rem] rounded-[1rem] font-semibold  hover:scale-[1.02] transition-all shadow-[0px_4px_19px_8px_#4042434d]'  >Upload Prescription 📄</Link>
             </div>
+            <div className='w-full mt-[4rem] mb-[8rem] ' >
+                <div className='flex w-full flex-wrap justify-around gap-y-[6rem] gap-x-[2rem]  ' >
+                    {
+                        MedData.map((i:any,index:number)=>{
+                            return(
+                                <div key={index} className='w-[20%] bg-gray-50 rounded-[10px] shadow-[0px_4px_19px_8px_#4042434d]  hover:scale-[1.03] transition-all ' >
+                                    <Image src={default_image} alt='default_image' className='w-full rounded-[10px] h-[40vh] object-cover '  />
+                                    <div className='flex gap-[0.5rem] p-[1rem] flex-col  ' >
+                                        <h2 className='text-[1.5rem] font-bold ' >{i.name}</h2>  
+                                        <h2 className='text-[1.2rem] text-gray-600 font-semibold '>{i.pack_size_label}</h2>      
+                                        <div className='flex justify-between items-center ' >
+                                            <h2 className='text-[1.5rem] text-green-500 font-bold' >₹ {i.price}</h2>
+                                            <button className='text-[1.3rem] font-bold bg-[#00beb1] px-[0.8rem] py-[0.5rem] rounded-[1rem] text-white cursor-pointer ' >Add To Cart</button>
+                                        </div>    
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className='mt-[8rem] flex justify-center gap-[5rem] ' >
+                    <button className='text-[1.5rem] font-bold bg-[#00beb1] px-[2rem] py-[0.5rem] rounded-[0.8rem] text-white cursor-pointer '>Prev Page</button>
+                    <button className='text-[1.5rem] font-bold bg-[#00beb1] px-[2rem] py-[0.5rem] rounded-[0.8rem] text-white cursor-pointer '>Next Page</button>
+                </div>
+            </div>
         </div>
+        <Footer/>
     </>
   )
 }
